057801b0476e6513fedb9ff4ba24f9ed85e7e6ca
JessKimSH <official1126@naver.com>
2019-04-09 16:33:59 +0900

    Section 3, Lecture 14

---
+++ src/main/java/com/github/simplejess/udemy/kafka/streams/StreamsStartApp.java
@@ -0,0 +1,53 @@
+package com.github.simplejess.udemy.kafka.streams;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KStreamBuilder;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.ValueMapper;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+public class StreamsStartApp {
+    public static void main(String[] args) {
+
+        // configuration
+        Properties config = new Properties();
+        config.setProperty(StreamsConfig.APPLICATION_ID_CONFIG, "word-count");
+        config.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        config.setProperty(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, String.valueOf(Serdes.String().getClass()));
+        config.setProperty(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, String.valueOf(Serdes.String().getClass()));
+
+        KStreamBuilder builder = new KStreamBuilder();
+        // 1 - stream from Kafka
+        KStream<String, String> wordCountInput = builder.stream("word-count-input");
+
+        // 2 - map values to lowercase
+        wordCountInput.mapValues(value -> value.toLowerCase());
+
+
+        KTable<String, Long> wordCounts = (KTable<String, Long>) wordCountInput
+                .mapValues(text -> text.toLowerCase())
+                // can be alternatively written as:
+                // .mapValues(String::toLowerCase)
+
+                // 3 - flatmap values split by space
+                .flatMapValues(value -> Arrays.asList(value.split(" ")))
+                // 4 - select key to apply a key (we discard the old key)
+                .selectKey((ignoredKey, value) -> value)
+                // 5 - group by key before aggregation
+                .groupByKey()
+                // 6 - count occurences
+                .count("Counts");
+
+        wordCounts.to(Serdes.String(), Serdes.Long(), "word-count-output");
+
+        KafkaStreams streams = new KafkaStreams(builder, config);
+        streams.start();
+    }
+}
